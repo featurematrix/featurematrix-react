b5978a8e3d680ddf3e7421386e8f74d6ad748d39
Nairi Narinyan <nairi.narinyan@gmail.com>
2019-06-05 18:45:04 +0400

    dont keep features in state

--- src/with-feature.tsx
+++ src/with-feature.tsx
@@ -9,62 +9,52 @@ export const createWithFeatures = (featureClient: FeatureMatrix) => <T extends o
     return class WithFeature extends Component<T & FeatureProps> {
         onReadySubscription: Subscription;
         onUpdateSubscription: Subscription;
+        featureKeys: string[];
+        mounted: boolean;
 
         constructor(props) {
             super(props);
 
-            this.state = featureKeys.reduce((acc, curr) => {
-                acc[curr] = null;
-                return acc;
-            }, {});
-
+            this.featureKeys = featureKeys;
             this.getFeatureState = this.getFeatureState.bind(this);
         }
 
         setupUpdateSubscription() {
-            return featureClient.on('update', feature => {
-                if (feature.key in this.state) {
-                    this.setState({
-                        [feature.key]: feature.isOn
-                    });
+            this.onUpdateSubscription = featureClient.on('update', feature => {
+                if (!this.mounted) return;
+                if (~this.featureKeys.indexOf(feature.key)) {
+                    this.forceUpdate();
                 }
             });
         };
 
         componentWillMount() {
             if (featureClient.initialized) {
-                this.getFeaturesAndSubscribe();
+                this.setupUpdateSubscription();
             } else {
                 this.onReadySubscription = featureClient.on('ready', () => {
-                    this.getFeaturesAndSubscribe();
+                    this.setupUpdateSubscription();
                 });
             }
         }
 
-        getFeaturesAndSubscribe() {
-            this.onUpdateSubscription = this.setupUpdateSubscription();
-
-            const featureStates = featureKeys
-                .map<[string, boolean]>(key => ([
-                    key,
-                    featureClient.getFeatureState(key)
-                ]))
-                .reduce((acc, curr) => {
-                    const [key, state] = curr;
-                    acc[key] = state;
-                    return acc;
-                }, {});
-
-            this.setState(featureStates);
+        componentDidMount() {
+            this.mounted = true;
         }
 
         componentWillUnmount() {
             this.onReadySubscription && this.onReadySubscription.unsubscribe();
-            this.onReadySubscription && this.onUpdateSubscription.unsubscribe();
+            this.onUpdateSubscription && this.onUpdateSubscription.unsubscribe();
+            this.mounted = false;
         }
 
         getFeatureState(key: string) {
-            return this.state[key];
+            try {
+                return featureClient.getFeatureState(key);
+            } catch (err) {
+                console.error(err);
+                return null;
+            }
         }
 
         render() {
